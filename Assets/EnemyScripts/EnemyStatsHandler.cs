84e7ecb65ce2807d74aa0eefa2870d3d897e985a
joduhee <engml9252@gmail.com>
2023-12-20 01:09:17 +0900

    enemyStatsHandler
    다시 수정

--- Assets/EnemyScripts/EnemyStatsHandler.cs
+++ Assets/EnemyScripts/EnemyStatsHandler.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum StatsChangeType
+public enum StatsChangeType 
 {
     Add,
     Overrider,
 }
 
-
-public class EnemyStatsHandler 
+[SerializeField]
+public class EnemyStatsHandler : MonoBehaviour
 {
     public StatsChangeType ChangeType;
    [Range(1,50)]public int maxHealth;
    [Range(1, 10)] public int speed;
+
+    public  EnemyObjectable enemyObjectable;
 
 }
