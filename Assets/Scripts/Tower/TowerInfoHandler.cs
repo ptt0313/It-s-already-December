e99cb46eb43da30804edff333f846fa4f68c535b
ptt0313 <149345231+ptt0313@users.noreply.github.com>
2023-12-19 21:00:14 +0900

    [feature][1219] 씬병합 및 테스트

--- Assets/Scripts/Tower/TowerInfoHandler.cs
+++ Assets/Scripts/Tower/TowerInfoHandler.cs
@@ -11,7 +11,7 @@ public class TowerInfoHandler : MonoBehaviour
 
     private void Awake()
     {
-        _towerSprite = GetComponentInChildren<Sprite>();
+        //_towerSprite = GetComponentInChildren<Sprite>();
     }
 
     private void Start()
