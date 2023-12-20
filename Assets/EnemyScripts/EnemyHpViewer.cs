d1aa97ad0a4660fd452a20d2828e04e5fb2006c8
joduhee <engml9252@gmail.com>
2023-12-20 12:23:14 +0900

    적 슬라이더 UI
    슬라이더 UI 생성 수정사항은 타워데미지를 추가할것!
    
    만든 스크립트는 EnemySliderHp, EnemyHpViewer, SliderEnemyHp prefab, WayPoint에 슬라이더UI따라다니게 설정

---
+++ Assets/EnemyScripts/EnemyHpViewer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHpViewer : MonoBehaviour
+{
+    private EnemyStatsHandler StatHandler;
+    private Slider hpSlider;
+
+    public void Setup(EnemyStatsHandler statsHandler)
+    {
+        StatHandler = statsHandler;
+        hpSlider = GetComponent<Slider>();
+    }
+}
+
+//    private void Update()
+//    {
+//        hpSlider.value = StatHandler.maxHealth -= 
+//}
