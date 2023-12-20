fe737541e123eac522161408444b74e8ee0d2767
ptt0313 <149345231+ptt0313@users.noreply.github.com>
2023-12-20 16:05:18 +0900

    [feature][1220] 팝업 복구,게임오버 레이어연동

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,41 +5,41 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject _victory;
+    public GameObject _defeat;
     public Text life;
     [SerializeField] private int Life = 3;
+
     private void Start()
     {
         UpdateLifeText();
     }
+
     private void UpdateLifeText()
     {
         life.text = Life.ToString();
     }
+
     public void DecreaseLife(int amount)
     {
         Life -= amount;
-        Debug.Log("체력감소");
+        Debug.Log("체력 감소");
         UpdateLifeText();
+
         if (Life <= 0)
         {
-            //라이프가 0이하가 됬을시 게임오버 부르기
+            // 라이프가 0 이하가 되었을 때 게임오버 호출
             GameOver();
         }
     }
+
     private void GameOver()
     {
-        // 종료팝업 불러오기
+        _defeat.SetActive(true);
+        Time.timeScale = 0f;
         Debug.Log("Game Over");
+        
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (gameObject.layer == LayerMask.NameToLayer("Enemy"))
-        {
-            Debug.Log("충돌");
-            Destroy(collision.gameObject);
-            DecreaseLife(1);
-        }
-    }
+
     private void OnDestroy()
     {
         // 파괴된 게임 오브젝트의 레이어가 Boss일 때 _victory를 활성화
@@ -49,9 +48,9 @@ public class GameManager : MonoBehaviour
             Invoke("GameVictory", 3f);
         }
     }
+
     private void GameVictory()
     {
-
         _victory.SetActive(true);
     }
 }
